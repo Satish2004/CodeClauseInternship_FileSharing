3c9198c77bb6b3fad4fd27e749dc8c17f734a470
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-03-06 16:15:23 +0530

    Update: New icons and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,19 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
+import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "File Box",
   description: "File Box is a file storage service.",
+  icons: {
+    icon: {
+      url: "/assets/icon.png",
+      href: "/assets/icon.png",
+    },
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClientProvider>
+          <Header />
+          {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
