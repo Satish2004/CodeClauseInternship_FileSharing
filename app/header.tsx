71d3ebad3f3eb28690609f2c61387594bd6092b5
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-03-12 22:08:41 +0530

    Dev: header with logo

---
+++ app/header.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import {
+  OrganizationSwitcher,
+  SignInButton,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="border-b py-3 bg-gray-50">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            src={"/assets/file-box-logo.png"}
+            alt="FileBox Logo"
+            width={65}
+            height={65}
+          />
+        </Link>
+        <div className="flex gap-x-4 items-center justify-center">
+          <div className="pt-1">
+            <OrganizationSwitcher />
+          </div>
+          <UserButton />
+          <SignedOut>
+            <SignInButton>
+              <Button>Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
